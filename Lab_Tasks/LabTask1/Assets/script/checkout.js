8763fb092654ae37bbcf024287eeabb2eb0cd047
Eman Butt <butt.eman391@gmail.com>
2025-03-14 16:53:33 +0500

    Username and email validation completed.

--- Lab_Tasks/LabTask1/Assets/script/checkout.js
+++ Lab_Tasks/LabTask1/Assets/script/checkout.js
@@ -2,7 +2,7 @@ function submitForm(event) {
     event.preventDefault();
 }
 
-function handleKeyDown(event, idName,divIdName) {
+function handleKeyDownForName(event, idName,divIdName) {
     if (event.key === "Enter") {
         event.preventDefault();
         validateName(idName, divIdName);
@@ -12,6 +12,42 @@ function handleKeyDown(event, idName,divIdName) {
 function validateName(idName,divIdName) {
     let nameToBeValidated = document.getElementById(idName).value;
     if (!isValidName(nameToBeValidated)) {
+        document.querySelector(divIdName+" .invalid-feedback").classList.add("display");
+        document.getElementById(idName).classList.remove("success");
+    } else {
+        document.querySelector(divIdName+" .invalid-feedback").classList.remove("display");
+        document.getElementById(idName).classList.add("success");
+    }
+}
+
+function handleKeyDownForUsername(event, idName,divIdName) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        validateUsername(idName, divIdName);
+    }
+}
+
+function validateUsername(idName,divIdName) {
+    let usernameToBeValidated = document.getElementById(idName).value;
+    if (!isValidUsername(usernameToBeValidated)) {
+        document.querySelector(divIdName+" .invalid-feedback").classList.add("display");
+        document.getElementById(idName).classList.remove("success");
+    } else {
+        document.querySelector(divIdName+" .invalid-feedback").classList.remove("display");
+        document.getElementById(idName).classList.add("success");
+    }
+}
+
+function handleKeyDownForEmail(event, idName,divIdName) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        validateEmail(idName, divIdName);
+    }
+}
+
+function validateEmail(idName,divIdName) {
+    let emailToBeValidated = document.getElementById(idName).value;
+    if (!isValidEmail(emailToBeValidated)) {
         document.querySelector(divIdName+" .invalid-feedback").classList.add("display");
         document.getElementById(idName).classList.remove("success");
     } else {
@@ -30,9 +66,29 @@ document.getElementById("lastName").addEventListener("input", function () {
     this.classList.remove("success");
 });
 
+document.getElementById("username").addEventListener("input", function () {
+    document.querySelector("#username-div .invalid-feedback").classList.remove("display");
+    this.classList.remove("success");
+});
+
+document.getElementById("email").addEventListener("input", function () {
+    document.querySelector("#email-div .invalid-feedback").classList.remove("display");
+    this.classList.remove("success");
+});
+
+
 function isValidName(name) {
     name = name.trim();
     var pattern = /^[A-Za-z' -]{2,50}$/;
     return pattern.test(name);
 }
 
+function isValidUsername(username) {
+    let pattern = /^(?!.*[_.]{2})[a-zA-Z0-9][a-zA-Z0-9._]{1,18}[a-zA-Z0-9]$/;
+    return pattern.test(username);
+}
+
+function isValidEmail(email) {
+    let pattern = /^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com|outlook\.com|hotmail\.com|icloud\.com|protonmail\.com)$/;
+    return pattern.test(email);
+}
