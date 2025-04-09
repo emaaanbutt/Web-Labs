905821b5b9df4b6fdf4c1fa00c46726aed472ea6
Eman Butt <butt.eman391@gmail.com>
2025-04-09 12:34:43 +0500

    Mid lab exam completed.

--- Exams/lab_midterm_exam/Assets/script/index.js
+++ Exams/lab_midterm_exam/Assets/script/index.js
@@ -1,11 +1,11 @@
-$(function(){
-    $("#previous-tasks").on("click", function(){
-        $("#tasks-list").toggle();
+$(function() {
+    $("#previous-tasks").on("click", function() {
+      $("#tasks-list").toggleClass("show");
     });
-
-    $("document").on("click", function(e){
-        if (!$(e.target).closest("#tasks-list".length)){
-            $("#tasks-list").hide();
-        }
+  
+    $(document).on("click", function(e) {
+      if (!$(e.target).closest("#tasks-list, #previous-tasks").length) {
+        $("#tasks-list").removeClass("show");
+      }
     });
-});
+  });
