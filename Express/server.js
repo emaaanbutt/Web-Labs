4fd8d146a3a4c497cf271f2e8f4b6b7f41562e4f
Eman Butt <butt.eman391@gmail.com>
2025-05-12 18:23:31 +0500

    Created women page.

--- Express/server.js
+++ Express/server.js
@@ -4,13 +4,18 @@ const port = 4000;
 const app = express();
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", (req,res)=>{
-    res.render("index.ejs");
+    res.render("index");
 });
 
 app.get("/checkout", (req,res) => {
-    res.render("checkout.ejs");
+    res.render("checkout");
+});
+
+app.get("/women", (req,res)=>{
+    res.render("women");
 });
 
 
