de02f674520c6c3ab894baba5b0c513685adb79f
Eman Butt <butt.eman391@gmail.com>
2025-05-13 13:28:01 +0500

    men page completed.

---
+++ Express/public/script/landing-page.js
@@ -0,0 +1,22 @@
+// $(function(){
+//     $("#women").on("click", function(event){
+//         event.preventDefault(); // Stop default for now
+//         $("#women").addClass("active");
+//         $("#men").removeClass("active");
+
+//         // Navigate after a short delay
+//         setTimeout(() => {
+//             window.location.href = $(this).attr("href");
+//         }, 100); // 100ms delay to show the active effect
+//     });
+
+//     $("#men").on("click", function(event){
+//         event.preventDefault();
+//         $("#men").addClass("active");
+//         $("#women").removeClass("active");
+
+//         setTimeout(() => {
+//             window.location.href = $(this).attr("href");
+//         }, 100); // same here
+//     });
+// });
